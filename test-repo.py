0d11d9e159a551f7ee6aab742aec8c092d767a5e
arcchatbot2017 <arcchatbot2017@gmail.com>
2017-05-30 16:40:32 -0400

    added database put

--- test-repo.py
+++ test-repo.py
@@ -5,6 +5,7 @@
 # A server is started byb Heroku and the bit above tells it to use python to run this code 
 
 # Here we import the packages needed for app 
+import boto3
 import urllib
 import json
 import os
@@ -13,44 +14,37 @@ from flask import Flask
 from flask import request
 from flask import make_response
 
+dynamodb = boto3.resource('dynamodb', region_name = 'us-east-1', aws_access_key_id= os.environ['ACCESS_KEY'],
+    aws_secret_access_key= os.environ['SECRET_KEY'])
+
+table = dynamodb.Table('chatbot_info')
+
 # Flask app should start in global layout
 app = Flask(__name__)
-
 
 # This is needed to beging Flask app --- POST is a method for transmitting data 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     # api.ai pushes json info to the app's url and req grabs the json 
     req = request.get_json(silent=True, force=True)
-    
     # all of the req.get instances pull data fromm specific variables from the json
-    sessionid = req.get("sessionId")
+    id1 = req.get('id')
+    sessionId = req.get("sessionId")
     timestamp = req.get("timestamp")
     result = req.get("result")
-    rquery = result.get("resolvedQuery")
+    query = result.get("resolvedQuery")
+    fulfillment = result.get('fulfillment')
     
-    # speech is simply a string and ".format" allows us to add the data that we want to sections with {}
-    speech = "My session ID is {}. The time stamp is {}. Your input was {}. We will add this to a database".format(sessionid, timestamp, rquery)
+    plus = {'id' = id1,
+           'sessionId': sessionId,
+           'timestamp': timestamp,
+           'result': result,
+           'query' : query
+           'fulfillment': fulfillment}
     
-    # this is a python dictionary with the speech string embedded in the speech and displyText varibiables  
-    out= {
-        "speech": speech,
-        "displayText": speech,
-        #"data": {},
-        #"contextOut": [],
-        "source": "test-api-app"
-    }
-
-    print("Request:")
-    print(json.dumps(req, indent=4))
-
-    res = json.dumps(out, indent=4)
-    print(res)
+    table.put_item(Item = plus)
     
-    # I think make_response converts the python to a json file with a header that can be interpreted on the other end  
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
+        
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
